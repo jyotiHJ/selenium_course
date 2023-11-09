ac3fc9ff401c6e360659f01c0eb103c5b3b515bb
Admin <Admin@DESKTOP-MFB6Q8O>
2023-11-09 09:31:23 +0530

    commit name code

---
+++ src/selenium_course/myname.java
@@ -0,0 +1,14 @@
+package selenium_course;
+
+public class myname {
+	public static void main (String[] args) {
+		char c1 = '\u091C';
+				char c2 = '\u094D';
+				char c3 = '\u092F';
+				char c4 = '\u094B';
+				char c5 = '\u0924';
+				char c6 = '\u0940';
+	System.out.println(c1+""+c2+""+c3+""+c4+""+c5+""+c6);
+	
+	}
+}
