1f5eec4017cc4498578ec503d1d222cb2d6d9233
Admin <Admin@DESKTOP-MFB6Q8O>
2023-11-09 08:53:26 +0530

    commit my code

---
+++ src/selenium_course/revise.java
@@ -0,0 +1,11 @@
+package selenium_course;
+
+public class revise {
+public static void main (String[] args) {
+	float f1 = 15.23F;
+	double d1 = 15.25d;
+	System.out.println(f1);
+	System.out.println(d1);
+}
+}
+
