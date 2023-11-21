4d6ce931d183c8c0fcd9b1f56d53061abead00f4
Admin <Admin@DESKTOP-MFB6Q8O>
2023-11-21 14:28:11 +0530

    commit

---
+++ src/selenium_course/FindFactors.java
@@ -0,0 +1,35 @@
+package selenium_course;
+
+import java.util.Scanner;
+
+public class FindFactors {
+
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+		 System.out.print("Enter the first integer: ");
+	        int num1 = scanner.nextInt();
+	        
+	        System.out.print("Enter the second integer: ");
+	        int num2 = scanner.nextInt();
+	        
+	        scanner.close();
+	       
+	        int sum = num1 + num2;
+	        int difference = num1 - num2;
+	        int product = num1 * num2;
+	        double average = (double) (num1 + num2);
+	        int distance = Math.abs(num1 - num2); 
+	        int maximum = Math.max(num1, num2);
+	        int minimum = Math.min(num1, num2);
+	        
+	        System.out.println("Sum: " + sum);
+	        System.out.println("Difference: " + difference);
+	        System.out.println("Product: " + product);
+	        System.out.println("Average: " + average);
+	        System.out.println("Distance: " + distance);
+	        System.out.println("Maximum: " + maximum);
+	        System.out.println("Minimum: " + minimum);   
+   
+	}
+	
+}
