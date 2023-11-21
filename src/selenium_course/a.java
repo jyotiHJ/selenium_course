189dca65f07bbc64a831077d1071742b8e42dfd8
Admin <Admin@DESKTOP-MFB6Q8O>
2023-11-21 14:35:09 +0530

    commit message

--- src/selenium_course/a.java
+++ src/selenium_course/a.java
@@ -6,17 +6,11 @@ public class a {
 	public static void main(String[] args) {
 	
 	
-		  System.out.println("Please Enter number:");
-		  Scanner scanner = new Scanner (System.in);
-		  int number = scanner.nextInt();
-		  
-		  if (number > 0) 
-		  {
-	            System.out.println("The entered number is positive.");
-	        } else 
-	        {
-	            System.out.println("The entered number is negative.");
-	        } 
+		int over = 1;
+		while(true) {
+		System.out.println("Over Number " + over + " is completed");
+			
+			}
 	        
 }
 }
