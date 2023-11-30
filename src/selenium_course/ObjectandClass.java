aa2685b532877657caa87e9a31da808eb6a07c2f
Admin <Admin@DESKTOP-MFB6Q8O>
2023-11-30 17:36:15 +0530

    commit

---
+++ src/selenium_course/ObjectandClass.java
@@ -0,0 +1,170 @@
+package selenium_course;
+
+public class ObjectandClass {
+
+	public static void main (String[] args) {
+		
+		class Spoon
+		{
+			String height;
+			String shape;
+			int size;
+			
+			void eat() {
+			
+			}
+		}
+		
+		class refrigerator
+		{
+			int weight;
+			String compressor;
+			String company;
+			Long Energy_Efficiency;
+			
+			void Cool() {
+				
+			}
+			
+		}
+		
+		class pen
+		{
+		
+		String type;
+		String color;
+		double Width;
+		
+		void write() {
+			
+		}
+		}
+		
+		class table
+		
+		{
+
+			double lenght;
+			String shape;
+			double width;
+			double height;
+			
+			void organize() {
+				
+			}
+			
+			
+			
+		}
+		
+		
+		class toothbrush
+		{
+			double lenght;
+			double width;
+			double height;
+			String material;
+			
+			void Cleaning_teeth() {
+				
+			}
+			
+			
+		}
+		
+		class books
+		{
+		    int pages;
+			String content;
+			double weight;
+			String material;
+			
+			void Reading() {
+			}
+			
+			
+		}
+		
+		class notebook
+		{
+			int size;
+			int pages;
+			String type;
+			String shape;
+			
+			void Write() {
+			
+		}
+		}
+		
+		class Mobile 
+		{
+			
+			String name;
+			int price;
+			String OS; 
+			String features;
+			
+			    void calling() {
+			
+			}
+		
+		}
+		
+		class clothes
+		{
+			
+			String color;
+			String material;
+			String strenght;
+			
+			void wear() {
+		}
+		}
+		
+		class Bag
+		{
+			 
+			String brand;
+			double size;
+			double weight;
+			
+			void carry_things() {
+			}
+	    }
+		
+	    class knife
+	    {
+	    	String material;
+	    	double weight;
+	    	double height;
+	    	 
+	    	void cutting() {
+	    		
+	    	}
+	    }  
+	
+		class car
+		{
+			String color;
+			String model;
+			int price;
+			
+			 void drive(){
+				 
+			 } 
+		}
+		
+		class Bed
+		{
+		    int weight;
+			double Height;
+			double Width;
+			  
+			void sleep() {
+				
+			}
+		}
+		
+	}
+}
