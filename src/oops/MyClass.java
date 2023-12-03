77602a60bd107f6316ba5e97e1a7a35934a00303
Admin <Admin@DESKTOP-MFB6Q8O>
2023-12-03 08:59:35 +0530

    commit

---
+++ src/oops/MyClass.java
@@ -0,0 +1,30 @@
+package oops;
+	
+		class Men {
+
+			void talk() {
+				System.out.println("Hello");
+			}
+		}
+
+		class Baby {
+
+			void cry() {
+				System.out.println("Bye");
+			}
+		}
+		
+		public class MyClass {
+		    public static void main(String[] args) {
+		        
+		        Men m1 = new Men();
+		        
+		    
+		        Baby b1 = new Baby();
+		        
+		        m1.talk();
+		        b1.cry();
+	} 
+} 
+
+
