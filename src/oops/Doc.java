f90eda2d5073c8bbb66062c60fa740ef06fede45
Admin <Admin@DESKTOP-MFB6Q8O>
2023-12-07 22:02:31 +0530

    commit

---
+++ src/oops/Doc.java
@@ -0,0 +1,21 @@
+package oops;
+
+public class Doc {
+
+	//Documentation
+	/**
+	 * This method is learn documentation comment in java. 
+	 * <BR/> This line i am writing for HTML presentation
+	 * @param i
+	 * @param j
+	 */
+      void commentMethod(int i, int j) {
+
+		int mul = i * j ;
+		System.out.println("Multiplication = " + mul);	
+	}
+
+}
+
+
+
---
+++ src/oops/DocMain.java
@@ -0,0 +1,13 @@
+package oops;
+
+public class DocMain {
+	public static void main(String[] args) {
+
+		Doc d1 = new Doc();
+
+		d1.commentMethod(5,6);
+
+
+
+	}
+}
