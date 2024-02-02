157515aa67ec2db9d741442b5ac50dcd7edb63bf
Admin <Admin@DESKTOP-MFB6Q8O>
2024-02-02 09:10:48 +0530

    commit

---
+++ src/oops/ACall.java
@@ -0,0 +1,18 @@
+package oops;
+
+class N {
+    static int k = m1();
+
+    public static int m1() {
+        return 10;
+    }
+
+	
+	
+	public static void main(String[] args) {
+		N.m1();
+		System.out.println(k);
+	}
+}
+
+
