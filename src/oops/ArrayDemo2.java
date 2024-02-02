e7d756c123983612380ceee922a6a7d49d7c61b9
Admin <Admin@DESKTOP-MFB6Q8O>
2024-02-02 08:52:34 +0530

    commit

---
+++ src/oops/ArrayDemo2.java
@@ -0,0 +1,36 @@
+package oops;
+
+public class ArrayDemo2 {
+
+	public static void main(String[] args) {
+		int[] student1 = new int[4];     //array using objects and given size 4
+
+		student1[0] = 50;
+		student1[1] = 45;
+		student1[2] = 36;
+		student1[3] = 60;
+
+		for (int i : student1) {
+			System.out.println(i);
+		}
+			
+			int[] school = new int[5];
+			
+			school[0] = 2;
+			school[1] = 2;
+			school[2] = 2;
+			school[3] = 2;
+			school[4] = 2;
+			
+			for (int k : school)
+			{
+				System.out.println(k);
+			}
+		
+			
+			
+		
+
+	}
+
+}
--- src/oops/MyClass.java
+++ src/oops/MyClass.java
@@ -1,30 +1,35 @@
 package oops;
 	
 		class Men {
+			
+			String name;
 
 			void talk() {
-				System.out.println("Hello");
+				System.out.println(name + ": Hello");
 			}
 		}
 
 		class Baby {
+			
+			String name;
 
 			void cry() {
-				System.out.println("Bye");
+				System.out.println(name+ ": Bye");
 			}
 		}
 		
+		
+
 		public class MyClass {
 		    public static void main(String[] args) {
 		        
 		        Men m1 = new Men();
-		        
+		        m1.name = "Arjun";
 		    
 		        Baby b1 = new Baby();
+		        b1.name= "Karna";
 		        
 		        m1.talk();
 		        b1.cry();
 	} 
 } 
-
-
