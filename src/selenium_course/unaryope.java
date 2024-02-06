6e10778f950d03f4e803575883f8f51846da2365
Admin <Admin@DESKTOP-MFB6Q8O>
2024-02-06 09:15:02 +0530

    Merge branch 'master' of https://github.com/jyotiHJ/selenium_course.git

---
+++ src/selenium_course/unaryope.java
@@ -0,0 +1,10 @@
+package selenium_course;
+
+public class unaryope {
+		public static void main(String[] args) {
+			System.out.println(-10);
+			boolean b1 = false;
+			System.out.println(!b1);
+		}
+
+}
